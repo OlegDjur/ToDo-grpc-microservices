18e6a31e51e8984494906994ec7e716e0523b59c
oleg <olegdjur@gmail.com>
2023-03-16 21:42:58 +0600

    created function LoadConfig for todo

--- todo/internal/config/config.go
+++ todo/internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port  string
+	DBUrl string
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+
+	return
+}
