88914e7ac320645d4a80d59a39276525aeca9aec
oleg <olegdjur@gmail.com>
2023-03-17 20:47:35 +0600

    created functions RegisterRoutes

--- gateway/internal/auth/delivery/grpc/routes.go
+++ gateway/internal/auth/delivery/grpc/routes.go
@@ -1,1 +1,18 @@
 package auth
+
+import (
+	"github.com/OlegDjur/go-grpc-api-gateway/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config) *ServiceClient {
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routes := r.Group("/auth")
+	routes.POST("/register", svc.Register)
+	routes.POST("/login", svc.Login)
+
+	return svc
+}
